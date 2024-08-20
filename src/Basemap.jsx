a292b952c03850c18e45a72354f38ef23379ad89
neriiacopo <neri.iacopo@gmail.com>
2024-08-20 10:40:30 +0200

    init

---
+++ src/Basemap.jsx
@@ -0,0 +1,9 @@
+export default function Basemap() {
+    return (
+        <iframe
+            className="basemap"
+            src="https://datathink-dt.github.io/2023-hypermap/"
+            title="Kepler basemap"
+        ></iframe>
+    );
+}
